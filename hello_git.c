99d45640f96476ad95f4581bb78bd706857d5668
Gerd Hofmann <gerd.hofmann.nbg@googlemail.com>
2012-09-09 04:13:30 +0200

    one more

---
+++ hello_git.c
@@ -0,0 +1,11 @@
+/* hello_git.c */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("****************\n");
+	printf("***Hello Git!***\n");
+	printf("****************\n");
+	return 0;
+}
+
